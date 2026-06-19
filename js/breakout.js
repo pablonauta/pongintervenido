194daf1b1f30bbaf444e2a4607311c3f10a9aaf4
pablonauta <pablonauta@gmail.com>
2026-06-19 13:28:11 -0300

    arranque

---
+++ js/breakout.js
@@ -0,0 +1,706 @@
+console.log('BREAKOUT');
+
+// Setup de ambiente
+const lienzo = document.getElementById('lienzo');
+const ctx = lienzo.getContext('2d');
+
+// Dimensiones del lienzo
+const { width, height } = lienzo; // Ancho y altura del lienzo
+
+// GameArea
+const HUDheight = 50;
+const gameMargin = 10;
+const gameArea = {
+    x1: gameMargin, y1: HUDheight,
+    x2: width - gameMargin, y2: height - gameMargin,
+};
+
+// Paleta de colores (PICO-8)
+const paleta = [
+	'#000000', '#1D2B53', '#7E2553', '#008751', '#AB5236', '#5F574F',
+	'#C2C3C7', '#FFF1E8', '#FF004D', '#FFA300', '#FFEC27', '#00E436',
+	'#29ADFF', '#83769C', '#FF77A8', '#FFCCAA',
+];
+
+// Vars globales del juego
+let vidas, puntos, finjuego = false;
+
+function cls ()
+{
+	ctx.clearRect(0, 0, width, height);
+}
+
+function rectFill (x1, y1, x2, y2, c)
+{
+	ctx.fillStyle = paleta[c];
+	ctx.fillRect(
+		x1, y1,									// Punto inicial
+		Math.max(x1, x2) - Math.min(x1, x2),	// Ancho
+		Math.max(y1, y2) - Math.min(y1, y2)		// Alto
+	);
+}
+
+function circFill (x, y, r, c)
+{
+	ctx.beginPath();
+	ctx.fillStyle = paleta[c];
+	ctx.arc(x, y, r, 0, Math.PI*2);
+	ctx.fill();
+	ctx.closePath();
+}
+
+function textFill (txt, x, y, c, style = 'bold 30px sans', align = 'left', baseline = 'top')
+{
+	ctx.font = style;
+	ctx.textAlign = align;
+	ctx.textBaseline = baseline;
+	ctx.fillStyle = paleta[c];
+
+	ctx.fillText(txt, x, y)
+}
+
+function textWidth (txt)
+{
+	ctx.font = 'bold 30px sans';
+	ctx.textAlign = 'left';
+	ctx.textBaseline = 'top';
+
+	return ctx.measureText(txt).width;
+}
+
+function colision (a, b)
+{
+    return !(
+        a.x + a.hitbox.x1 > b.x + b.hitbox.x2
+        || a.x + a.hitbox.x2 < b.x + b.hitbox.x1
+        || a.y + a.hitbox.y1 > b.y + b.hitbox.y2
+        || a.y + a.hitbox.y2 < b.y + b.hitbox.y1
+    );
+}
+
+let ent_paleta = [];
+
+function showPaleta (mostrar = true)
+{
+    if (mostrar)
+    {
+        for (let i = 0, l = 16; i < l; i++)
+        {
+            const e = new ColorPaleta();
+            e.width = 50;
+            e.height = 50;
+            e.color = i;
+
+            e.x = (i % 4) * 50;
+            e.y = Math.floor(i / 4) * 50;
+
+            ent_paleta.push(e);
+        }
+    }
+    else
+    {
+        for (const e of ent_paleta)
+        {
+            e.remove();
+        }
+
+        ent_paleta = [];
+    }
+}
+
+class Control
+{
+    // Singleton
+    static instance = null;
+
+    // Teclas
+    static flechaDerecha = false;
+    static flechaIzquierda = false;
+    static enter = false;
+
+    constructor ()
+    {
+        window.addEventListener('keydown',  Control.manageControl);
+        window.addEventListener('keyup',    Control.manageControl);
+    }
+
+    static getInstance ()
+    {
+        if (Control.instance === null)
+        {
+            Control.instance = new Control();
+        }
+
+        return Control.instance;
+    }
+
+    static manageControl (evt)
+    {
+        switch (evt.code)
+        {
+            case 'ArrowRight':
+                Control.flechaDerecha = evt.type === 'keydown';
+                break;
+
+            case 'ArrowLeft':
+                Control.flechaIzquierda = evt.type === 'keydown';
+                break;
+
+            case 'Enter':
+                Control.enter = evt.type === 'keydown';
+                break;
+
+            default:
+                break;
+        }
+    }
+}
+
+class Entidad
+{
+	static entidades = [];
+    #width = 10;
+    #height = 10;
+
+	constructor ()
+	{
+		this.x = 0;
+		this.y = 0;
+		this.color = 0;
+
+        this.velocidad = 0;
+        this.dx = 0;
+        this.dy = 0;
+
+        this.hitbox = {
+            x1: 0, y1: 0,
+            x2: 0, y2: 0,
+        };
+
+		Entidad.entidades.push(this);
+	}
+
+    get width ()
+    {
+        return this.#width;
+    }
+
+    set width (val)
+    {
+        this.#width = val;
+        this.hitbox.x2 = val;
+
+        return val;
+    }
+
+    get height ()
+    {
+        return this.#height;
+    }
+
+    set height (val)
+    {
+        this.#height = val;
+        this.hitbox.y2 = val;
+        return val;
+    }
+
+	update ()
+	{
+        this.x += this.dx;
+        this.y += this.dy;
+	}
+
+	draw ()
+	{
+		rectFill(
+			this.x, 
+			this.y, 
+			this.x + this.width,
+			this.y + this.height,
+			this.color
+		);
+
+        if (this.debug)
+        {
+            ctx.strokeStyle = '#00ff00';
+            ctx.strokeRect(
+                this.x + this.hitbox.x1, 
+                this.y + this.hitbox.y1, 
+                this.hitbox.x2 - this.hitbox.x1, 
+                this.hitbox.y2 - this.hitbox.y1, 
+            );
+        }
+	}
+
+    remove ()
+    {
+        const ind = Entidad.entidades.indexOf(this);
+        Entidad.entidades.splice(ind, 1);
+    }
+}
+
+class ColorPaleta extends Entidad
+{
+    draw ()
+    {
+        super.draw();
+
+        textFill(this.color, this.x + 10, this.y + 20, 
+        this.color == 7 ? 0 : 7);
+    }
+}
+
+class Jugador extends Entidad
+{
+    #canMove = true;
+
+    constructor ()
+    {
+        super();
+
+        this.width = 120;
+        this.height = 20;
+        this.color = 9;
+
+        this.posicionInicial();
+    }
+
+    posicionInicial ()
+    {
+        this.x = (gameArea.x2 - gameArea.x1) / 2 + gameArea.x1 - this.width / 2;
+        this.y = gameArea.y2 - this.height;
+
+        this.#canMove = false;
+
+        setTimeout(_ => {
+            this.#canMove = true;
+        }, 1000);
+    }
+
+    update ()
+    {
+        this.dx = 0;
+
+        if (this.#canMove)
+        {
+            if (Control.flechaDerecha) this.dx = this.velocidad;
+            if (Control.flechaIzquierda) this.dx += -this.velocidad;
+        }
+
+        super.update();
+
+        if (this.x <= gameArea.x1)
+        {
+            this.x = gameArea.x1;
+            this.dx = 0;
+        }
+
+        if (this.x >= gameArea.x2 - this.width)
+        {
+            this.x = gameArea.x2 - this.width;
+            this.dx = 0;
+        }
+    }
+
+    onColision (ent)
+    {
+        this.color = this.color === 9 ? 14 : 9;
+    }
+}
+
+class Ladrillo extends Entidad
+{
+    #puntos = 10;
+
+    constructor (w = 100, h = 30, vida = 1)
+    {
+        super();
+
+        this.width = w;
+        this.height = h;
+
+        this.vida = vida;
+        this.color = [
+            0, 12, 11, 10, 8
+        ][ vida ];
+    }
+
+    get puntos ()
+    {
+        return this.#puntos;
+    }
+
+    set puntos (val)
+    {
+        this.#puntos = val;
+        return val;
+    }
+
+    onColision (ent)
+    {
+        this.vida -= 1;
+
+        if (this.vida <= 0)
+        {
+            puntos += this.#puntos;
+
+            this.remove();
+        }
+
+        this.color = [
+            0, 12, 11, 10, 8
+        ][ this.vida ];
+    }
+}
+
+class Pelota extends Entidad
+{
+    #radio = 10;
+    #jugador = null;
+
+    constructor ()
+    {
+        super();
+
+        super.width = this.#radio * 2;
+        super.height = this.#radio * 2;
+
+        this.color = 1;
+
+        this.posicionInicial();
+    }
+
+    get jugador ()
+    {
+        return this.#jugador;
+    }
+
+    set jugador (val)
+    {
+        this.#jugador = val;
+
+        return val;
+    }
+
+    get width ()
+    {
+        return super.width;
+    }
+
+    set width (val)
+    {
+        super.width = val;
+        super.height = val;
+        this.#radio = val / 2;
+
+        return val;
+    }
+
+    get height ()
+    {
+        return super.height;
+    }
+
+    set height (val)
+    {
+        super.width = val;
+        super.height = val;
+        this.#radio = val / 2;
+
+        return val;
+    }
+
+    get radio ()
+    {
+        return this.#radio;
+    }
+
+    set radio (val)
+    {
+        this.#radio = val;
+
+        super.width = this.#radio * 2;
+        super.height = this.#radio * 2;
+
+        return val;
+    }
+
+    posicionInicial ()
+    {
+        this.x = (gameArea.x2 - gameArea.x1) / 2 + gameArea.x1 - this.#radio;
+        this.y = gameArea.y2 - this.height * 2.2;
+    }
+
+    resetMovimiento ()
+    {
+        const dx = 4 * (Math.random() < .5 ? -1 : 1);
+        const dy = -4;
+
+        this.dx = 0;
+        this.dy = 0;
+
+        const fn = _ => {
+            this.dx = dx;
+            this.dy = dy;
+        };
+
+        setTimeout(fn, 2000);
+    }
+
+    update ()
+    {
+        // Update de la entidad
+        // Cambia la posición de la pelota
+        super.update();
+        
+        // Rebote con escenario en horizontal
+        if (this.x <= gameArea.x1 || this.x >= gameArea.x2 - this.width)
+        {
+            this.dx *= -1;
+        }
+
+        // Rebote con escenario en vertical
+        if (this.y <= gameArea.y1)
+        {
+            this.dy *= -1;
+        }
+
+        // ¿La pelota está tocando "abajo"?
+        if (this.y >= gameArea.y2 - this.height)
+        {
+            // Acá se perdió una vida
+            vidas -= 1;
+
+            if (vidas <= 0)
+            {
+                // Se terminó el juego
+                this.#jugador.remove();
+                this.remove();
+
+                finjuego = true;
+            }
+            else
+            {
+                // Reset de escenario
+                this.posicionInicial();
+                this.resetMovimiento();
+
+                this.#jugador.posicionInicial();
+            }
+
+            return;
+        }
+
+        for (const e of Entidad.entidades)
+        {
+            // ¿Esta entidad, es la pelota? Si es así, saltear la verificación
+            if (e === this)
+            {
+                continue; // Saltea este paso del ciclo
+            }
+
+            if (colision(this, e))
+            {
+                // Verificar verticalmente
+                let pdy = this.dy;
+                this.y -= pdy; // Movemos para atrás un paso
+
+                if (!colision(this, e)) this.dy *= -1;
+
+                this.y += pdy;
+
+                // Verificar horizontalmente
+                let pdx = this.dx;
+                this.x -= pdx;
+
+                if (!colision(this, e)) this.dx *= -1;
+
+                this.x += pdx;
+
+                if (e.onColision !== undefined)
+                {
+                    e.onColision(this);
+                }
+            }
+        }
+    }
+
+    draw ()
+    {
+        circFill(
+            this.x + this.#radio,
+            this.y + this.#radio,
+            this.#radio,
+            this.color
+        );
+
+
+        if (this.debug)
+        {
+            ctx.strokeStyle = '#00ff00';
+            ctx.strokeRect(
+                this.x + this.hitbox.x1, 
+                this.y + this.hitbox.y1, 
+                this.hitbox.x2 - this.hitbox.x1, 
+                this.hitbox.y2 - this.hitbox.y1, 
+            );
+        }
+    }
+}
+
+function hud ()
+{
+    ctx.strokeStyle = paleta[13];
+    ctx.strokeRect(
+        gameArea.x1, gameArea.y1,
+        gameArea.x2 - gameArea.x1,
+        gameArea.y2 - gameArea.y1
+    );
+
+    for (let i = 0; i < 4; i++)
+    {
+        textFill("Breakout", 
+            10 + 2 * i, 
+            18 - 2 * i, 
+            11 - i);
+    }
+
+    // Puntos
+    textFill(
+        "PUNTOS", 
+        (gameArea.x2 - gameArea.x1) / 2 + gameArea.x1,
+        gameArea.y1 - 2,
+        5,
+        'bold 11px sans',
+        'center',
+        'bottom');
+
+    textFill(
+        puntos,
+        (gameArea.x2 - gameArea.x1) / 2 + gameArea.x1,
+        gameArea.y1 - 14,
+        12,
+        'bold 25px sans',
+        'center',
+        'bottom'
+    );
+
+    // Vidas
+    let strvidas = '';
+
+    for (let i = 0; i < vidas; i++)
+    {
+        strvidas +=  '❤️';
+    }
+
+    textFill(
+        strvidas,
+        gameArea.x2,
+        gameArea.y1 - 5,
+        0,
+        'bold 28px sans',
+        'end',
+        'bottom'
+    );
+
+    if (finjuego)
+    {
+        ctx.fillStyle = '#0004'; // Negro con transparencia
+        ctx.fillRect(
+            0, 0,
+            width, height
+        );
+
+        ctx.fillStyle = paleta[8];
+        ctx.fillRect(
+            0, height / 2 - 40,
+            width, 80
+        );
+
+        textFill(
+            "FIN DEL JUEGO",
+            width / 2,
+            height / 2,
+            7,
+            'bold 40px sans',
+            'center',
+            'middle'
+        );
+    }
+}
+
+function init ()
+{
+    Control.getInstance();
+
+    const j = new Jugador();
+    j.velocidad = 5;
+
+    const p = new Pelota();
+    p.jugador = j;
+    p.resetMovimiento();
+
+    // p.dx = 4 * (Math.random() < .5 ? -1 : 1);
+    // p.dy = -4;
+
+    const cantCols = 7;
+    const cantFilas = 4;
+    const div = cantCols - 1;
+    const gap = 10;
+    const padding = 10;
+    const lw = (gameArea.x2 - gameArea.x1 - gap * div - padding * 2) / cantCols;
+    const lh = 20;
+
+    for (let j = 0; j < cantFilas; j++)
+    {
+        const filay = gameArea.y1 + padding + (lh + gap) * j;
+
+        for (let i = 0; i < cantCols; i++)
+        {
+            const e = new Ladrillo(lw, lh, cantFilas - j);
+            e.x = (lw + gap) * i + padding + gameArea.x1;
+            e.y = filay;
+            e.puntos = e.vida * 10;
+        }
+    }
+
+    // Vars globales
+    vidas = 3;
+    puntos = 0;
+
+	gameloop();
+}
+
+function update ()
+{
+	for (const e of Entidad.entidades)
+	{
+		e.update();
+	}
+}
+
+function draw ()
+{
+    cls();
+    
+	for (const e of Entidad.entidades)
+	{
+		e.draw();
+	}
+
+    // HUD
+    hud();
+}
+
+function gameloop ()
+{
+	update();
+	draw();
+
+    // console.log(Control.flechaDerecha, Control.flechaIzquierda, Control.enter);
+
+	requestAnimationFrame(gameloop);
+}
+
+init();
